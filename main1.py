ce16464e9a6800beabeb0fc91cd54ab7f5e4e917
koany <koroliev@yandex.ru>
2023-07-02 11:59:11 +0300

    второе задание готово

--- main1.py
+++ main1.py
@@ -1,77 +1,88 @@
 import random
 
-# klass = int(input('Выберите класс персонажа:\n1 - Воин\n2 - Лучник\n3 - Маг\n'))
-# race = int(input('Выберите расу персонажа:\n1 - Эльф\n2 - Орк\n3 - Человек\n'))
-# name = input('Дайте персонажу имя:\n')
-#
+stats_sum = 30
 
-klass = 3
-name = ''
+klass = int(input('Выберите класс персонажа:\n1 - Воин\n2 - Лучник\n3 - Маг\n'))
+race = int(input('Выберите расу персонажа:\n1 - Эльф\n2 - Орк\n3 - Человек\n'))
+name = input('Дайте персонажу имя:\n')
+
 hero = {'klass': '', 'race': '', 'name': name,
-        'stats': {'strength': 0, 'stamina': 0, 'agility': 0, 'charisma': 0, 'intelligence': 0, 'luck': 0}}
-#
-#
-#
-# match race:
-#     case 1: hero['race'] = 'эльф'
-#     case 2: hero['race'] = 'орк'
-#     case 3: hero['race'] = 'человек'
+        'stats': {'сила': 0, 'выносливость': 0, 'ловкость': 0, 'харизма': 0, 'интеллект': 0, 'удача': 0}}
+
+match race:
+    case 1: hero['race'] = 'Эльф'
+    case 2: hero['race'] = 'Орк'
+    case 3: hero['race'] = 'Человек'
 
 
-
+# Создаём массив со значениями статов
+# Заполняем его случайными значениями,
+# нормально распределёнными с матожиданием 5 и среднеквадратичным отклонением 5/3
 stats = []
-sum = 30
 for i in range(6):
-    stats.append(round(random.gauss(5, 5/3)))
+    stats.append(abs(round(random.gauss(5, 5/3))))
 
-
-print(stats)
-print(sorted(stats))
-
+# сортируем получившийся массив, чтобы максимальные значения оказались в конце (элементы [4] и [5])
 stats = sorted(stats)
-res = 0
+
+# считаем сумму статов в массиве
+stats_cur_sum = 0
 for i in stats:
-    res += i
+    stats_cur_sum += i
 
-while res < sum:
+# если сумма меньше заданной (30),
+# то прибавляем 1 к предпоследнему максимальному значению,
+# увеличиваем сумму и снова сортируем
+# пока сумма не станет 30
+while stats_cur_sum < stats_sum:
     stats[4] += 1
-    res += 1
+    stats_cur_sum += 1
     stats = sorted(stats)
 
-while res > sum:
+# если сумма больше заданной (30),
+# то вычитаем 1 из пред-предпоследнего максимального,
+# уменьшаем сумму и снова сортируем
+# пока сумма не станет 30
+while stats_cur_sum > stats_sum:
     stats[3] -= 1
-    res -= 1
+    stats_cur_sum -= 1
     stats = sorted(stats)
 
-if stats[3] == stats[4]:
+# если пред-предпоследнее и предпоследнее максимальные равны
+# меняем между ними единицу, чтобы было отличие между двумя максимальными и четырьмя минимальными
+# сортируем и проверяем заново
+while stats[3] == stats[4]:
     stats[3] -= 1
     stats[4] += 1
+    stats = sorted(stats)
 
-
-
-print(stats, res)
-
+# вычленяем 4 минимальных и 2 максимальных значений массива статов
+# перемешиваем два получившихся массива
 stats_low = stats[:4]
 stats_high = stats[4:]
 random.shuffle(stats_low)
 random.shuffle(stats_high)
 
-
-
-print(stats, res)
+# в зависимости от класса вписываем название в характеристики героя
+# и переформируем массив статов в нужном порядке
 match klass:
     case 1:
-        hero['klass'] = 'воин'
+        hero['klass'] = 'Воин'
         stats = stats_high + stats_low
     case 2:
-        hero['klass'] = 'лучник'
+        hero['klass'] = 'Лучник'
         stats = stats_low[:2] + stats_high + stats_low[2:]
     case 3:
-        hero['klass'] = 'маг'
+        hero['klass'] = 'Маг'
         stats = stats_low + stats_high
 
+# выводим информацию о персонаже
+print('Ваш персонаж:', hero['name'], hero['klass'], hero['race'])
+
+# вписываем статы из массива в словарь характеристик героя и выводим характеристики
 i = 0
 for st in hero['stats']:
     hero['stats'][st] = stats[i]
     i += 1
     print(st.ljust(15), hero['stats'][st])
+
